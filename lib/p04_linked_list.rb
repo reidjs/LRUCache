e6934c840648033b5d625b3e8e9b855a61052d75
reid <reidsherman@gmail.com>
2017-12-29 19:26:41 -0800

    Linked list appends nodes

--- lib/p04_linked_list.rb
+++ lib/p04_linked_list.rb
@@ -15,11 +15,17 @@ class Node
   def remove
     # optional but useful, connects previous node to next node
     # and removes self from list.
+    @prev.next = @next 
+    @next.prev = @prev
   end
 end
 
 class LinkedList
   def initialize
+    @head = Node.new 
+    @tail = Node.new 
+    @tail.prev = @head 
+    @head.next = @tail
   end
 
   def [](i)
@@ -28,27 +34,57 @@ class LinkedList
   end
 
   def first
+    @head.next
   end
 
   def last
+    @tail.prev
   end
 
   def empty?
+    @head.next == @tail
   end
 
   def get(key)
+    node = first 
+    while node 
+      return node.val if node.key == key
+      node = node.next 
+    end 
+    nil
+  end 
+
+  def find(key)
+    i = 0
+    node = first 
+    while node
+      return node if node.key == key
+      node = node.next 
+    end 
+    nil
   end
 
   def include?(key)
   end
 
   def append(key, val)
+    node = Node.new(key, val)
+    temp_node = last
+    @tail.prev = node 
+    # last = node 
+    node.next = @tail 
+    temp_node.next = node 
+    node.prev = temp_node 
   end
 
   def update(key, val)
+    node = find(key)
+    node.val = val if node 
   end
 
   def remove(key)
+    node = find(key)
+    node.remove
   end
 
   def each
