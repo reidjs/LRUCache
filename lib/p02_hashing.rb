4a5da50f1a0094677c2c6b9cba741118f4f7d693
reid <reidsherman@gmail.com>
2017-12-29 18:42:03 -0800

    Create hash, array, and string hashing functions

--- lib/p02_hashing.rb
+++ lib/p02_hashing.rb
@@ -4,11 +4,19 @@ end
 
 class Array
   def hash
-  end
+    #Order matters 
+    sum = 0
+    self.each_with_index do |el, idx|
+    sum += el * 2**idx if el.class == Fixnum 
+    sum += el.ord * 2**idx if el.class == String
+    end 
+    sum.hash #hash function on fixnum. The sum should be good enough tbh, but may have collisions
+  end 
 end
 
 class String
   def hash
+    self.chars.hash 
   end
 end
 
@@ -16,6 +24,12 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    #stable order the keys 
+    keys = self.keys 
+    values = self.values
+    keys.sort!
+    values.sort!
+    arr = keys.concat(values)
+    arr.hash
   end
 end
